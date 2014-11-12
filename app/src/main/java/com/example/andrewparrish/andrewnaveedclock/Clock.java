1c0040c638a030c5a056236feaef9cbc0c0d5cd1
Naveed Nadjmabadi <naveedn@terpmail.umd.edu>
2014-11-11 22:42:23 -0500

    implements basic clock view -- task 1

--- app/src/main/java/com/example/andrewparrish/andrewnaveedclock/Clock.java
+++ app/src/main/java/com/example/andrewparrish/andrewnaveedclock/Clock.java
@@ -2,16 +2,61 @@ package com.example.andrewparrish.andrewnaveedclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Clock extends Activity {
 
+    public TextView textViewDate;
+    public TextView textViewTime;
+    public RelativeLayout parentFrame;
+    private Timer timer;
+    private TimerTask timerTask;
+    private static final String LOG = "clock-activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_clock);
+        parentFrame = (RelativeLayout) findViewById(R.id.parentFrame);
+        textViewDate = (TextView) findViewById(R.id.textViewDate);
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
+
+        //instantiate the time object necessary for the clock
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(500);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Time today = new Time(Time.getCurrentTimezone());
+                                today.setToNow();
+                                // update TextView here!
+                                setTime(today);
+                                setDate(today);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
     }
 
 
@@ -32,5 +77,17 @@ public class Clock extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setTime(Time today){
+        String time = today.format("%k:%M:%S");
+        textViewTime.setText(today.format("%k:%M:%S"));  // Current time
+        Log.i(LOG, time);
+    }
+
+    private void setDate(Time today){
+        String date = today.month + "/" + today.monthDay + "/" + today.year + "";
+        textViewDate.setText(date);
+        Log.i(LOG, date);
     }
 }
